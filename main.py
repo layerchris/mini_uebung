7d693f3ada260c000e422b0df5263e0aa3633c11
Moritz Kronegger <s54204@edu.campus02.at>
2024-11-11 14:27:03 +0100

    Funktion Rabatt

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-print("test")
+bestellung = [["Duschgel", 10, 4], ["Rasierschaum", 2, 3]]
+
+def berechne_gesamtpreis_mit_rabatt(bestellung):
+    gesamtpreis = 0
+
+    for artikel in bestellung:
+        name, menge, preis_pro_stueck = artikel
+
+        if menge >= 10:
+            rabatt = 0.10
+        elif menge >= 5:
+            rabatt = 0.05
+        else:
+            rabatt = 0.0
+
+        preis_nach_rabatt = preis_pro_stueck * (1 - rabatt)
+        gesamtpreis_artikel = menge * preis_nach_rabatt
+
+        gesamtpreis += gesamtpreis_artikel
+    print(gesamtpreis)
+    return gesamtpreis
+
+
+berechne_gesamtpreis_mit_rabatt(bestellung)
+
+
 
