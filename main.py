4f0e54c13e0a0d8367118801576cdb7ab892ad22
Christoph Layer <s55117@edu.campus02.at>
2024-11-27 19:10:14 +0100

    added Steuer Funktion

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 bestellung = [["Duschgel", 10, 4], ["Rasierschaum", 2, 3]]
 
+def steuern_hinzufuegen(bestellung: list) -> list:
+    for b in bestellung:
+        if b[0].lower().startswith(tuple("abcdefghijk")):
+            b[2] = round(b[2]*1.1,2)
+        else:
+            b[2] = round(b[2]*1.2,2)
 
-
-
-
-
+    return bestellung
