ae849aa0e02f17f95782e31baeee3e3a756bd440
Moritz Kronegger <s54204@edu.campus02.at>
2024-11-27 19:05:17 +0100

    Reset main (no branches)

--- main.py
+++ main.py
@@ -1,49 +1,7 @@
 bestellung = [["Duschgel", 10, 4], ["Rasierschaum", 2, 3]]
 
-def berechne_gesamtpreis_mit_rabatt(bestellung):
-    gesamtpreis = 0
-
-    for artikel in bestellung:
-        name, menge, preis_pro_stueck = artikel
-
-        if menge >= 10:
-            rabatt = 0.10
-        elif menge >= 5:
-            rabatt = 0.05
-        else:
-            rabatt = 0.0
-
-        preis_nach_rabatt = preis_pro_stueck * (1 - rabatt)
-        gesamtpreis_artikel = menge * preis_nach_rabatt
-
-        gesamtpreis += gesamtpreis_artikel
-    print(gesamtpreis)
-    return gesamtpreis
-
-
-berechne_gesamtpreis_mit_rabatt(bestellung)
 
 
 
 
-def berechne_bestellungssumme(artikel_liste):
-    gesamtbetrag = 0
-    for artikel in artikel_liste:
-        artikel_name = artikel['name']
-        preis_pro_artikel = artikel['preis']
-        anzahl_artikel = artikel['anzahl']
 
-        # Rabattberechnung
-        preis_mit_rabatt = berechne_rabatt(preis_pro_artikel, anzahl_artikel)
-
-        # Steuerberechnung
-        steuer = berechne_steuer(artikel_name, preis_mit_rabatt)
-
-        # Portokostenberechnung
-        portokosten = berechne_portokosten(artikel_name, anzahl_artikel)
-
-        # Gesamtsumme für diesen Artikel
-        artikel_summe = preis_mit_rabatt + steuer + portokosten
-        gesamtbetrag += artikel_summe
-
-    return gesamtbetrag
